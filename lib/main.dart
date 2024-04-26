46e3829cefff9adfc101d7e74cb10e7f10ae14a2
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-04-26 10:10:50 +0530

    Bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Text("Metric"),
+                Text("Imperial"),
+              ],
+            ),
+            title: const Text('BMI App'),
+          ),
+          body: const TabBarView(
+            children: [BMICalculator(), BMICalculator1()],
+          ),
+        ),
       ),
-      home: BMICalculator(),
     );
   }
 }
@@ -37,10 +50,10 @@ class _BMICalculatorState extends State<BMICalculator> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI Calculator'),
+        title: const Text('BMI Calculator'),
       ),
       body: Container(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Column(
           children: [
             TextField(
@@ -72,9 +85,9 @@ class _BMICalculatorState extends State<BMICalculator> {
                   showBmi = true;
                 });
               },
-              child: Text(
+              child: const Text(
                 'Calculate',
-                style: const TextStyle(
+                style: TextStyle(
                   fontSize: 30,
                 ),
               ),
@@ -96,3 +109,187 @@ class _BMICalculatorState extends State<BMICalculator> {
     );
   }
 }
+
+class Menu extends StatelessWidget {
+  const Menu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+              ],
+            ),
+            title: const Text('Menu'),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BMICalculator1 extends StatefulWidget {
+  const BMICalculator1({super.key});
+
+  @override
+  State<BMICalculator1> createState() => _BMICalculatorState1();
+}
+
+class _BMICalculatorState1 extends State<BMICalculator1> {
+  TextEditingController heightController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+
+  double bmi = 0.0;
+
+  bool showBmi = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI Calculator'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter Height in Foot/Inch',
+              ),
+              controller: heightController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter Weight in Pound',
+              ),
+              controller: weightController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  double height = double.parse(heightController.text);
+                  double weight = double.parse(weightController.text);
+                  bmi = ((weight * 0.453592) /
+                          ((height * 30.48) * (height * 30.48))) *
+                      10000;
+                  showBmi = true;
+                });
+              },
+              child: const Text(
+                'Calculate',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            showBmi
+                ? Text(
+                    "bmi: " + bmi.toStringAsFixed(2),
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  )
+                : const SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// class BottomNavigationBarExampleApp extends StatelessWidget {
+//   const BottomNavigationBarExampleApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: BottomNavigationBarExample(),
+//     );
+//   }
+// }
+
+// class BottomNavigationBarExample extends StatefulWidget {
+//   const BottomNavigationBarExample({super.key});
+
+//   @override
+//   State<BottomNavigationBarExample> createState() =>
+//       _BottomNavigationBarExampleState();
+// }
+
+// class _BottomNavigationBarExampleState
+//     extends State<BottomNavigationBarExample> {
+//   int _selectedIndex = 0;
+//   static const TextStyle optionStyle =
+//       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+//   static const List<Widget> _widgetOptions = <Widget>[
+//     Text(
+//       'Index 0: Home',
+//       style: optionStyle,
+//     ),
+//     Text(
+//       'Index 1: Business',
+//       style: optionStyle,
+//     ),
+//   ];
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('BottomNavigationBar Sample'),
+//       ),
+//       body: Center(
+//         child: _widgetOptions.elementAt(_selectedIndex),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Home',
+//             backgroundColor: Colors.red,
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.business),
+//             label: 'Business',
+//             backgroundColor: Colors.green,
+//           ),
+//         ],
+//         currentIndex: _selectedIndex,
+//         selectedItemColor: Colors.amber[800],
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
+
+// class TabController extends StatelessWidget {}
